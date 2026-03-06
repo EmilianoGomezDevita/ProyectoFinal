7c2c639dd9b156446aa6d3806fbf5007423b0339
Emi <egomezdevita@gmail.com>
2026-03-06 09:45:12 -0300

    Contador de productos agregados e implementacion de boton Quitar

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,19 @@
 console.log("JS conectado")
 
+//mensaje al apretar el boton comprar en productis.html
 const botonesCompra = document.querySelectorAll(".btn-comprar");
 const toast = document.getElementById("toast");
+const contadorProd = document.getElementById("carritoContador")
+
+console.log(contadorProd)
+
+let contador = 0;
 
 botonesCompra.forEach(boton => {
     boton.addEventListener("click", () => {
         // Reiniciamos la animación si ya estaba corriendo
         toast.classList.remove("show");
         void toast.offsetWidth; // Truco para resetear animaciones CSS
-        
         toast.classList.add("show");
         
         // La animación de CSS se encarga de ocultarlo, 
@@ -16,5 +21,27 @@ botonesCompra.forEach(boton => {
         setTimeout(() => {
             toast.classList.remove("show");
         }, 3000);
+        
+        contador = contador + 1;
+        contadorProd.textContent = "Productos en carrito 🛒: " + contador;
+
     });
 });
+
+botonesCompra.forEach(boton => {
+    boton.addEventListener("clik", function aumentarContador(){
+        contador++;
+        contadorProd.textContent = contador;
+    });
+
+})
+
+//contador de productos
+
+
+
+contadorProd.addEventListener("click", () => {
+    if(contadorProd){
+        contador++;
+    }
+})
